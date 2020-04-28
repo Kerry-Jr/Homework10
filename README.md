c20b80cdc91fb34b523a4b1f74b4632d5221d1d6
Kerry Smith <kerrysfs@gmail.com>
2020-04-27 22:28:20 -0700

    fixed func AE

---
+++ README.md
@@ -0,0 +1,40 @@
+
+![Github avatar](https://avatars3.githubusercontent.com/u/59150488?v=4)
+
+## Username
+## Kerry-Jr
+# Title
+#### # Unit 12 MySQL Homework: Employee Tracker
+# Table of contents:
+1. Description
+1. Installation
+1. Usage
+1. Test
+1. Contributors
+##### Description
+1.) Developers are often tasked with creating interfaces that make it easy for non-developers to view and interact with information stored in databases. Often these interfaces are known as **C**ontent **M**anagement **S**ystems. In this homework assignment, your challenge is to architect and build a solution for managing a company's employees using node, inquirer, and MySQL.Build a command-line application that at a minimum allows the user to:
+##### Installation
+2.) npm i
+##### Usage
+3.) Tracking software for large corporations.
+##### Test
+4.) run npm i and see if it works.
+##### Contributors
+5.) Study groups, and my tutor.
+##### Licenses
+6.) Licensed under the [MIT](https://spdx.org/licenses/MIT.html).
+##### Email
+7.)[My Email. Please click me to send a message](mailto:kerrysfs@gmail.com)
+
+![Image of Kerry's App](./assets/image/app.gif)
+
+
+[Github repo link](https://github.com/Kerry-Jr "Your github repo")
+  
+
+  
+ 
+  
+
+  
+
--- app.js
+++ app.js
@@ -47,10 +47,10 @@ function mainMenu() {
         updateEmpRole();
         break;
       case "Exit":
-      connection.end();  
-      return;
+        connection.end();
+        return;
       default:
-      return;
+        return;
     }
   })
 }
@@ -59,8 +59,8 @@ function mainMenu() {
 async function viewDepartments() {
   try {
     const query = 'SELECT * FROM departments';
-    const [ departments ] = await connection.query(query);
-    console.table("\x1b[31m",departments);
+    const [departments] = await connection.query(query);
+    console.table("\x1b[31m", departments);
     mainMenu();
   } catch (error) {
     console.log(error)
@@ -70,7 +70,7 @@ async function viewDepartments() {
 async function viewRoles() {
   try {
     const query = 'SELECT * FROM roles';
-    const [ roles ] = await connection.query(query);
+    const [roles] = await connection.query(query);
     console.table("\x1b[32m", roles);
     mainMenu();
   } catch (error) {
@@ -81,7 +81,7 @@ async function viewRoles() {
 async function viewEmployees() {
   try {
     const query = 'SELECT * FROM employees';
-    const [ employees ] = await connection.query(query);
+    const [employees] = await connection.query(query);
     console.table("\x1b[33m", employees);
     mainMenu();
   } catch (error) {
@@ -91,23 +91,24 @@ async function viewEmployees() {
 
 function addDepartment() {
   inquirer
-      .prompt(
-          {
-              name: 'deptTitle',
-              message: "What is the department's name?",
-              type: 'input'
-          }
-      ).then(function ({ deptTitle }) {
-          connection.query(`INSERT INTO departments (deptTitle) VALUES ('${deptTitle}')`, function (err, data) {
-              if (err) throw err;
-              console.log(`NEW DEPARTMENT HAS BEEN SUCCESSFULLY ADDED`)
-              mainMenu();
-          }) 
-          try {
-          } catch (e) {
-              console.log(e)
-          }
+    .prompt(
+      {
+        name: 'deptTitle',
+        message: "What is the department's name?",
+        type: 'input'
+      }
+    ).then(function ({ deptTitle }) {
+      connection.query(`INSERT INTO departments (deptTitle) VALUES ('${deptTitle}')`, function (err, data) {
+        if (err) throw err;
+        viewDepartments();
+        console.log(`NEW DEPARTMENT HAS BEEN SUCCESSFULLY ADDED`)
+        mainMenu();
       })
+      try {
+      } catch (e) {
+        console.log(e)
+      }
+    })
 }
 
 function addRole() {
@@ -121,38 +122,91 @@ function addRole() {
       {
         type: 'input',
         name: 'salary',
-        message: 'Please add salary for role.' 
+        message: 'Please add salary for role.'
       },
       {
         type: 'input',
         name: 'department_id',
         message: 'Which dept id would you to add to this role?'
       }
-    ]).then(function ({ title, salary, department_id }) {
+    ]).then(function (answers) {
       // let deptIndex = department_id.indexOf(department_id);
-      connection.query(`INSERT INTO roles (title, salary, department_id) VALUES ('${title}', '${salary}', ${department_id})`, function (err, data) {
+      console.log(answers.title);
+      console.log(answers.salary);
+      console.log(answers.department_id)
+      connection.query(`INSERT INTO roles (title, salary, department_id) VALUES ('${answers.title}', '${answers.salary}', ${answers.department_id})`, function (err, data) {
         if (err) throw err;
-        console.table(${title});
-        
-        // console.log(`New role successfully added  New Title: ('${title}') New Salary: ${salary}  Added to department ${department_id}.`);
-        // console.log(`...returning to main menu`)
+        viewRoles();
         mainMenu();
+      })
     })
-})
 }
 
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "What is the first name of the new employee?"
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "What is the last name? of the new employee?"
+      },
+      {
+        type: "number",
+        name: "role_id",
+        message: "What is the new employees role?"
 
-async function addEmployee() {
-
+      },
+      {
+        type: "number",
+        name: "manager_id",
+        message: "Who is their manager?"
+      }
+    ])
+    .then(function (answers) {
+       
+      connection.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('${answers.first_name}', '${answers.last_name}', '${answers.role_id}', '${answers.manager_id}')`),
+      viewEmployees();
+      mainMenu();
+    })
 }
- 
-
-
-
-
 
 async function updateEmpRole() {
- 
+  connection.query("SELECT first_name, last_name, id FROM employees",
+    function (err, res) {
+      if (err) throw err;
+      let employees = res.map(employee => ({ name: employee.first_name + " " + employee.last_name, value: employee.id }));
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "employees",
+            message: "Which employee's role would you like to update?",
+            choices: employees
+          },
+          {
+            type: "input",
+            name: "role",
+            message: "What is your new role?"
+          }
+        ])
+        .then(function (answers) {
+          connection.query(`UPDATE employees SET role_id = '${answers.role}' WHERE id = '${answers.employees}'`,
+
+            function (err, res) {
+              if (err) throw err;
+              viewEmployees();
+              console.log(`Employee's role has been updated successfully. Thank You`);
+              mainMenu();
+            }
+          );
+        })
+    }
+  )
 }
 
 mainMenu();
